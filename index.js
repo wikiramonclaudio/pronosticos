624776f7c02c8645df23ff540847587f1f89543c
Jose vázquez Prada <makarenosh@gmail.com>
2019-08-24 13:06:12 +0200

    Change mongo db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.PORT || 7000;
+var port = process.env.PORT || 8080;
 
 
 mongoose.connection.openUri('mongodb://upmcxcuogynl6hm9cvuq:AllxwAaK3xWx7Gc5I7kl@bxpoufqrax40wky-mongodb.services.clever-cloud.com:27017/bxpoufqrax40wky', { useNewUrlParser: true }, (error, res)=>{
@@ -12,5 +12,5 @@ mongoose.connection.openUri('mongodb://upmcxcuogynl6hm9cvuq:AllxwAaK3xWx7Gc5I7kl
 });
 
 app.listen(port, ()=>{
-    console.log('Servidor Node/Express corriendo en puerto 7000: \x1b[36m%s\x1b[0m', 'online');
+    console.log('Servidor Node/Express corriendo en puerto 8080: \x1b[36m%s\x1b[0m', 'online');
 });
