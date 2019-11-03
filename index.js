f87269647ba644415059124880ef6aaedd7322da
Jose vázquez Prada <makarenosh@gmail.com>
2019-11-03 15:04:16 +0100

    change to prod env

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var app = require('./app');
 var port = process.env.PORT || 8080;
 
 
-// mongoose.connection.openUri('mongodb://upmcxcuogynl6hm9cvuq:AllxwAaK3xWx7Gc5I7kl@bxpoufqrax40wky-mongodb.services.clever-cloud.com:27017/bxpoufqrax40wky', { useNewUrlParser: true }, (error, res)=>{
-    mongoose.connection.openUri('mongodb://localhost:27017/pronosticos', { useNewUrlParser: true }, (error, res)=>{    
+mongoose.connection.openUri('mongodb://upmcxcuogynl6hm9cvuq:AllxwAaK3xWx7Gc5I7kl@bxpoufqrax40wky-mongodb.services.clever-cloud.com:27017/bxpoufqrax40wky', { useNewUrlParser: true }, (error, res)=>{
+    // mongoose.connection.openUri('mongodb://localhost:27017/pronosticos', { useNewUrlParser: true }, (error, res)=>{    
 if(error)
         console.log(error);
         console.log('conectado a la DB pronosticos en mongoDB');
